d10703c9ca96428953b7f94b36c63b7c9329994b
Weining Hu <weininghu@Jiaxings-MacBook-Pro.local>
2018-04-28 16:30:08 -0700

    Rename to pyBluzelle

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='bluzelleHackathon',
+setup(name='pyBluzelle',
       version='0.1',
       description='Python implementation for bluzelle APIs',
       url='http://github.com/storborg/funniest',
       author='krxsky, liangjiaxing, weininghu1012, exeex',
       author_email='weininghu1012@gmail.com',
       license='MIT',
-      packages=['bluzelleHackathon'],
+      packages=['pyBluzelle'],
       install_requires=['websocket-client>=0.47.0'],
       zip_safe=False)
