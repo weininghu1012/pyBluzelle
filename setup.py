e1f0d8d6c6efe0dbb718ebc0963e33b0c0382991
Weining Hu <weininghu@Jiaxings-MacBook-Pro.local>
2018-04-28 15:32:19 -0700

    Add basic structure for python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='bluzelleHackathon',
+      version='0.1',
+      description='Python implementation for bluzelle APIs',
+      url='http://github.com/storborg/funniest',
+      author='krxsky, liangjiaxing, weininghu1012, exeex',
+      author_email='weininghu1012@gmail.com',
+      license='MIT',
+      packages=['bluzelleHackathon'],
+      install_requires=['websocket>=0.47.0']
+      zip_safe=False)
