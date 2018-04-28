c237b92f97bf6efe1a25a00d19518b4e192b5162
cswu <xray0h@gmail.com>
2018-04-28 16:44:39 -0700

    update class structure

---
+++ pyBluzelle/main.py
@@ -0,0 +1,48 @@
+from websocket import create_connection
+import json
+
+
+# example form:
+"""
+data = {
+    "bzn-api": "crud",
+    "cmd": None,
+    "data": {
+        "key": None,
+        "value": None
+    },
+    "db-uuid": None,
+    "request-id": None
+}
+"""
+
+
+
+class Bluzelle:
+
+    def __init__(self):
+
+        pass
+
+    def connect(self,ip,uuid):
+
+        pass
+
+    def create(self,key,value):
+        pass
+
+    def update(self,key,value):
+        pass
+
+    def read(self,key):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def has(self, key):
+        pass
+
+    def keys(self):
+        pass
+
