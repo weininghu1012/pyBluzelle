b3ead725fc3b456f88f63a46ab077a1b863af2d7
Jiaxing Liang <liangjiaxing57@gmail.com>
2018-04-28 19:33:20 -0700

    add redirect

--- test/test_create.py
+++ test/test_create.py
@@ -1,7 +1,7 @@
 from pyBluzelle.main import Bluzelle
 import time
 b = Bluzelle()
-b.connect("ws://127.0.0.1:51010","137a8403-52ec-43b7-8083-91391d4c5e67")
+b.connect("127.0.0.1", 51011, "137a8403-52ec-43b7-8083-91391d4c5e67")
 b.create("gg","1234")
 time.sleep(1)
 b.read("gg")
