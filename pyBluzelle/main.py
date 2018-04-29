80c73d2a72264e6c94c8695f4ba8c3d4d10234da
Jiaxing Liang <liangjiaxing57@gmail.com>
2018-04-29 12:37:22 -0700

    implement has, keys and delete

--- pyBluzelle/main.py
+++ pyBluzelle/main.py
@@ -35,43 +35,56 @@ class Bluzelle:
             "request-id": 0
         }
 
-    def create(self,key,value):
+    def create(self, key, value):
         req = self.payload
         req["cmd"] = "create"
         req["data"]["key"] = key
         req["data"]["value"] = value
         # test if the workder nodes all get updated
-        self.__sendRequest(req)
-        self.__poll(self.read, key, value)
+        ret = self.__sendRequest(req)
+        self.__poll(lambda : self.read(key) == value, key)
+        return ret
 
-    def update(self,key,value):
+
+    def update(self, key, value):
         req = self.payload
         req["cmd"] = "update"
         req["data"]["key"] = key
         req["data"]["value"] = value
-        self.__sendRequest(req)
-        self.__poll(self.read, key, value)
+        ret = self.__sendRequest(req)
+        self.__poll(lambda : self.read(key) == value, key)
+        return ret
 
 
-    def read(self,key):
+    def read(self, key):
         req = self.payload
         req["cmd"] = "read"
         req["data"]["key"] = key
-        return self.__sendRequest(req)
+        ret = self.__sendRequest(req)
+        if "data" in ret:
+            return ret["data"]["value"]
+        return None
 
     def delete(self, key):
         req = self.payload
         req["cmd"] = "delete"
         req["data"]["key"] = key
-        return self.__sendRequest(req)
+        ret = self.__sendRequest(req)
+        self.__poll(lambda : self.read(key) == None, key)
+        return ret
 
     def has(self, key):
-        pass
+        req = self.payload
+        req["cmd"] = "read"
+        req["data"]["key"] = key
+        return self.__sendRequest(req)
 
     def keys(self):
-        
+        req = self.payload
+        req["cmd"] = "keys"
+        return self.__sendRequest(req)
 
-    def __poll(self, operation, key, value):
+    def __poll(self, operation, key):
         pollRate = 200
         pollTimeout = 2000
         sleepTime = 0.1
@@ -79,11 +92,10 @@ class Bluzelle:
 
         startTime = current_milli_time()
         while (current_milli_time() - startTime < pollTimeout):
-            updatedObjectDic = operation(key)
-            if ("data" in updatedObjectDic and updatedObjectDic["data"]["value"] == value):
+            if (operation()):
                 return 
             time.sleep(sleepTime)
-            sleepTime = sleepTime*2
+            sleepTime = sleepTime * 2
         raise TimeoutError("Time out for this operation")
 
     def __sendRequest(self, data):
@@ -94,7 +106,7 @@ class Bluzelle:
         d = eval(ret)
         if 'error' in d and d['error'] == 'NOT_THE_LEADER':
             self.ip = 'ws://' + self.host + ':' + str(d['data']['leader-port'])
-            self.__sendRequest(data)
+            d = self.__sendRequest(data)
         return d
 
 
